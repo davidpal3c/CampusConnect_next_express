e25f415b24d697cfb3df6149d3bf95f1ba2586fa
Jaeeun <jaeeun.lee@edu.sait.ca>
2025-02-03 18:46:39 -0700

    Student Sidebar color changed

--- client/src/app/components/Sidebar/UserSidebarBbutton.tsx
+++ client/src/app/components/Sidebar/UserSidebarBbutton.tsx
@@ -10,7 +10,7 @@ type SidebarButtonProps = {
 
 export default function UserSidebarButton(props: SidebarButtonProps) {
   return (
-    <div className="flex items-center text-md m-3 p-3 pl-6 w-48 rounded-xl hover:border hover:border-saitLighterBlueOg text-saitWhite stroke-saitWhite fill-saitWhite hover:bg-saitPurple hover:text-saitWhite hover:fill-saitWhite hover:stroke-saitWhite transition duration-300 ease-out">
+    <div className="flex items-center text-md m-3 p-3 pl-6 w-48 rounded-xl hover:border hover:border-saitLighterBlueOg text-saitWhite stroke-saitWhite fill-saitWhite hover:bg-saitBlueOg hover:text-saitWhite hover:fill-saitWhite hover:stroke-saitWhite transition duration-300 ease-out">
       <props.icon className="size-5 mr-5" />
       <Link href={props.href}>{props.children}</Link>
     </div>
