2de85d8481ca0b1c90d467e7a50c6bfa13e88328
David <davidpal3c@gmail.com>
2025-03-18 12:30:47 -0600

    articles(audience): current audience criteria fix

--- client/src/app/components/PageComponents/Admin/Articles/AudienceSelectionModal.tsx
+++ client/src/app/components/PageComponents/Admin/Articles/AudienceSelectionModal.tsx
@@ -1,75 +1,87 @@
 "use client";
-import React, { useEffect, useState } from 'react';
 
+import React, { useEffect, useState } from 'react';
 import ActionButton from '@/app/components/Buttons/ActionButton';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import BeenhereRoundedIcon from '@mui/icons-material/BeenhereRounded';
 import ScrollableList from '@/app/components/PageComponents/Admin/Articles/ScrollableList';
 import ScrollableListForObjects, { Item } from '@/app/components/PageComponents/Admin/Articles/ScrollableListForObjects';
-
 import { toast } from "react-toastify";
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { IconButton } from "@mui/material";
 import { Tooltip } from '@mui/material';
 
-
 type AudienceSelectionModalProps = {
-    openAudienceSelectionModal: any,
-    setOpenAudienceSelectionModal: any,
-    saveAudienceCriteria: any,  
-    currentAudienceCriteria?: any,
-    setShowAccordion: any,  
+    openAudienceSelectionModal: boolean;
+    setOpenAudienceSelectionModal: (open: boolean) => void;
+    saveAudienceCriteria: (criteria: any) => void;
+    currentAudienceCriteria?: any;
+    setShowAccordion: (show: boolean) => void;
 };
 
-export default function AudienceSelectionModal({ 
-    openAudienceSelectionModal, setOpenAudienceSelectionModal, 
-    saveAudienceCriteria, currentAudienceCriteria, setShowAccordion }: AudienceSelectionModalProps) {
+export default function AudienceSelectionModal({
+    openAudienceSelectionModal,
+    setOpenAudienceSelectionModal,
+    saveAudienceCriteria,
+    currentAudienceCriteria,
+    setShowAccordion,
+}: AudienceSelectionModalProps) {
 
     const [isSaved, setIsSaved] = useState(false);
 
     const [selectedPrograms, setSelectedPrograms] = useState<Item[]>([]);
     const [selectedDepartments, setSelectedDepartments] = useState<Item[]>([]);
     const [selectedIntakeSeasons, setSelectedIntakeSeasons] = useState<string[]>([]);
-    const [selectedIntakeYears, setSelectedIntakeYears] = useState<string[]>([]);       // convert fetched numbers to string (to match the 'All' option)
-    
+    const [selectedIntakeYears, setSelectedIntakeYears] = useState<string[]>([]);
+
     const [isLoading, setIsLoading] = useState(false);
     const [availableAudience, setAvailableAudience] = useState<any>({});
-
+    
     const [programsData, setProgramsData] = useState<Item[]>([]);
     const [departmentsData, setDepartmentsData] = useState<Item[]>([]);
     const [intakeSeasons, setIntakeSeasons] = useState<string[]>([]);
     const [intakeYears, setIntakeYears] = useState<string[]>([]);
-    // const { programs = [], departments = [], intakeSeasons = [], intakeYears = [] } = availableAudience;
 
+    useEffect(() => {
+        fetchAvailableAudience();
+    }, []);
+
+    useEffect(() => {
+        if (currentAudienceCriteria) {
+        setSelectedPrograms(currentAudienceCriteria.programs || []);
+        setSelectedDepartments(currentAudienceCriteria.departments || []);
+        setSelectedIntakeSeasons(currentAudienceCriteria.intakeSeasons || []);
+        setSelectedIntakeYears(currentAudienceCriteria.intakeYear || []);
+        }
+    }, [currentAudienceCriteria]);
 
     const fetchAvailableAudience = async () => {
         try {
             setIsLoading(true);
+
             const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/audience/`, {
                 method: "GET",
                 headers: {
-                  "content-type": "application/json",
+                "content-type": "application/json",
                 },
-                credentials: "include",   
+                credentials: "include",
             });
 
             const audienceData = await response.json();
 
-            if(!response.ok) {
+            if (!response.ok) {
                 const errorData = audienceData;
                 toast.error(errorData.message || "An Error occurred fetching audience.");
                 return;
             }
 
             setAvailableAudience(audienceData);
-            
             setProgramsData(audienceData.data.programs);
             setDepartmentsData(audienceData.data.departments);
             setIntakeSeasons(audienceData.data.intakeSeasons);
             setIntakeYears(audienceData.data.intakeYears);
-
-            console.log(audienceData.message);
+            
         } catch (error: any) {
             console.error("Error fetching Audience Selection: ", error);
             toast.error("Error fetching Audience Selection", error);
@@ -81,60 +93,40 @@ export default function AudienceSelectionModal({
     const handleAudienceSelectionClose = () => {
         if (!isSaved) {
             saveAudienceCriteria({});
-            setOpenAudienceSelectionModal(false);
-        } 
-        setOpenAudienceSelectionModal(false)
+        }
+
+        setOpenAudienceSelectionModal(false);
     };
 
     const handleSave = async () => {
         const audienceCriteria = {
-          programs: selectedPrograms,
-          departments: selectedDepartments,
-          intakeSeasons: selectedIntakeSeasons.includes('All') ? ['All'] : selectedIntakeSeasons,
-          intakeYear: selectedIntakeYears.includes('All') ? ['All'] : selectedIntakeYears,
+        programs: selectedPrograms,
+        departments: selectedDepartments,
+        intakeSeasons: selectedIntakeSeasons.includes('All') ? ['All'] : selectedIntakeSeasons,
+        intakeYear: selectedIntakeYears.includes('All') ? ['All'] : selectedIntakeYears,
         };
 
-        saveAudienceCriteria({...audienceCriteria});              //ensure reactivity: pass a new object reference to trigger re-render
-        
+        saveAudienceCriteria(audienceCriteria);
         setIsSaved(true);
-        handleAudienceSelectionClose();
         setShowAccordion(true);
+        handleAudienceSelectionClose();
     };
 
-    // Handle selection for Programs
     const handleProgramSelection = (programs: Item[]) => {
-        setSelectedPrograms(programs); 
-    };
-  
-  // Handle selection for Departments
-    const handleDepartmentSelection = (departments: Item[]) => {
-        setSelectedDepartments(departments); 
+        setSelectedPrograms(programs);
     };
 
-    // Handle selection for Intake Season
+    const handleDepartmentSelection = (departments: Item[]) => {
+        setSelectedDepartments(departments);
+    };
+
     const handleIntakeSeasonSelection = (seasons: string[]) => {
         setSelectedIntakeSeasons(seasons);
     };
 
-    // Handle selection for Intake Year
     const handleIntakeYearSelection = (years: string[]) => {
         setSelectedIntakeYears(years);
     };
-
-    useEffect(() => {
-        fetchAvailableAudience();
-    }, []);
-
-
-
-    useEffect(() => {
-        console.log("Current Audience Criteria (modal): ", currentAudienceCriteria);
-        setProgramsData(currentAudienceCriteria?.programs || []);
-        setDepartmentsData(currentAudienceCriteria?.departments || []); 
-        setIntakeSeasons(currentAudienceCriteria?.intakeSeasons || []);
-        setIntakeYears(currentAudienceCriteria?.intakeYear || []);
-    }, [currentAudienceCriteria]);
-
 
     return (
         <div>
@@ -144,90 +136,82 @@ export default function AudienceSelectionModal({
                 aria-labelledby="audience-selection-modal"
                 aria-describedby="audience-selection-modal-description"
                 BackdropProps={{
-                    sx: { backgroundColor: 'rgba(0, 0, 0, 0.5)' }
+                sx: { backgroundColor: 'rgba(0, 0, 0, 0.5)' }
                 }}
             >
                 <Box sx={modalStyle}>
-                    <div className="flex flex-col items-center justify-center w-auto">
-                        <div className="flex flex-row items-center justify-between p-4 mt-1 w-full">
-                            <div></div>
-                            <div>
-                                <h1 className="text-xl font-bold">Select Audience</h1>
-                            </div>
-                            <div className="">
-                                <Tooltip title="Close">
-                                    <IconButton onClick={handleAudienceSelectionClose}>
-                                        <CloseRoundedIcon />
-                                    </IconButton>
-                                </Tooltip>
-                            </div>
-                        </div>
+                <div className="flex flex-col items-center justify-center w-auto">
+                    <div className="flex flex-row items-center justify-between p-4 mt-1 w-full">
+                    <div></div>
+                    <div>
+                        <h1 className="text-xl font-bold">Select Audience</h1>
+                    </div>
+                    <div className="">
+                        <Tooltip title="Close">
+                            <IconButton onClick={handleAudienceSelectionClose}>
+                                <CloseRoundedIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                    </div>
 
-                        <div className="w-full">
-                            {isLoading ? (
-                                <p>Loading Audience...</p>
-                            ) : programsData?.length > 0 || departmentsData?.length > 0 || intakeSeasons?.length > 0 || intakeYears?.length > 0 ? (
-                                <div className='flex flex-row items-center justify-center px-6 my-2 w-full gap-4'>
-                                    
-                                    {/* Departments List */}
-                                    <div className="flex-1">
-                                        <label className="block text-md font-semibold mb-2">Departments</label>
-                                        <ScrollableListForObjects
-                                            items={departmentsData?.map((d) => ({ department_id: d.department_id, name: d.name })) || []}
-                                            selectedItems={selectedDepartments}
-                                            onSelect={handleDepartmentSelection}
-                                            idObjKey="department_id"
-                                            currentAudienceCriteria={currentAudienceCriteria?.departments}
-                                        />
-                                    </div>
-                                    
-                                    {/* Programs List */}
-                                    <div className="flex-1">
-                                        <label className="block text-md font-semibold mb-2">Programs</label>
-                                        <ScrollableListForObjects
-                                            items={programsData?.map((p) => ({ program_id: p.program_id, name: p.name })) || []}
-                                            selectedItems={selectedPrograms}
-                                            onSelect={handleProgramSelection}
-                                            idObjKey="program_id"
-                                            currentAudienceCriteria={currentAudienceCriteria?.programs}
-                                        />
-                                    </div>
-
-                                    {/* Intake Season List */}
-                                    <div className="w-1/6">
-                                        <label className="block text-md font-semibold mb-2">Intake Season</label>
-                                        <ScrollableList
-                                            items={intakeSeasons?.map((season) => season) || []}
-                                            selectedItems={selectedIntakeSeasons}
-                                            onSelect={handleIntakeSeasonSelection}
-                                            includeAllOption={true}
-                                            currentAudienceCriteria={currentAudienceCriteria?.intakeSeasons}
-                                        />
-                                    </div>
-
-                                    {/* Intake Year List */}
-                                    <div className="w-1/6">
-                                        <label className="block text-md font-semibold mb-2">Intake Season</label>
-                                        <ScrollableList
-                                            items={intakeYears?.map((y) => y.toString()) || []}
-                                            selectedItems={selectedIntakeYears}
-                                            onSelect={handleIntakeYearSelection}
-                                            includeAllOption={true}
-                                            currentAudienceCriteria={currentAudienceCriteria?.intakeYear}
-                                        />
-                                    </div>
-                                </div>
-                            ) : (
-                                <p>No Audience Data Available</p>
-                            )}
-                        </div>
-
-                        <div className="my-5">
-                            <ActionButton onClick={handleSave} type="button" title="Save Selection" icon={<BeenhereRoundedIcon sx={{ fontSize: 20 }} />}
-                                bgColor="bg-saitWhite" textColor="text-saitBlue" borderColor="border-saitBlue" hoverBgColor="bg-saitBlue" hoverTextColor="text-saitWhite"
+                    <div className="w-full">
+                    {isLoading ? (
+                        <p>Loading Audience...</p>
+                    ) : programsData?.length > 0 || departmentsData?.length > 0 || intakeSeasons?.length > 0 || intakeYears?.length > 0 ? (
+                        <div className='flex flex-row items-center justify-center px-6 my-2 w-full gap-4'>
+                        <div className="flex-1">
+                            <label className="block text-md font-semibold mb-2">Departments</label>
+                            <ScrollableListForObjects
+                                items={departmentsData}
+                                selectedItems={selectedDepartments}
+                                onSelect={handleDepartmentSelection}
+                                idObjKey="department_id"
+                                currentAudienceCriteria={currentAudienceCriteria?.departments}
                             />
                         </div>
+                        <div className="flex-1">
+                            <label className="block text-md font-semibold mb-2">Programs</label>
+                            <ScrollableListForObjects
+                                items={programsData}
+                                selectedItems={selectedPrograms}
+                                onSelect={handleProgramSelection}
+                                idObjKey="program_id"
+                                currentAudienceCriteria={currentAudienceCriteria?.programs}
+                            />
+                        </div>
+                        <div className="w-1/6">
+                            <label className="block text-md font-semibold mb-2">Intake Season</label>
+                            <ScrollableList
+                                items={intakeSeasons}
+                                selectedItems={selectedIntakeSeasons}
+                                onSelect={handleIntakeSeasonSelection}
+                                includeAllOption={true}
+                                currentAudienceCriteria={currentAudienceCriteria?.intakeSeasons}
+                            />
+                        </div>
+                        <div className="w-1/6">
+                            <label className="block text-md font-semibold mb-2">Intake Year</label>
+                            <ScrollableList
+                                items={intakeYears}
+                                selectedItems={selectedIntakeYears}
+                                onSelect={handleIntakeYearSelection}
+                                includeAllOption={true}
+                                currentAudienceCriteria={currentAudienceCriteria?.intakeYear}
+                            />
+                        </div>
+                        </div>
+                    ) : (
+                        <p>No Audience Data Available</p>
+                    )}
                     </div>
+
+                    <div className="my-5">
+                        <ActionButton onClick={handleSave} type="button" title="Save Selection" icon={<BeenhereRoundedIcon sx={{ fontSize: 20 }} />}
+                            bgColor="bg-saitWhite" textColor="text-saitBlue" borderColor="border-saitBlue" hoverBgColor="bg-saitBlue" hoverTextColor="text-saitWhite"
+                        />
+                    </div>
+                </div>
                 </Box>
             </Modal>
         </div>
@@ -251,28 +235,7 @@ const modalStyle = {
     borderRadius: 4,
     boxShadow: 2,
     backgroundColor: "#f7f7f7",
-    // backgroundColor: "#ffffff",
 };
-
-
-const customButton = "group text-saitDarkRed border border-saitDarkRed bg-saitWhite font-normal h-9 flex flex-row items-center justify-center h-9 py-2 px-4 rounded-full hover:bg-saitDarkRed hover:text-white hover:shadow-2xl active:scale-75 transition delay-150 transition-colors transition-transform duration-300 ease-in-out";
-
-
-
-// {
-//     "programs": [
-//         {"name": "Software Development", "program_id": "SADT-009"},
-//         {"name": "Business Administration Diploma", "program_id": "SADT-012"}
-//     ],
-//     "intakeYear": ["2023", "2024", "2025"],
-//     "departments": [
-//         {"name": "School of Advanced Digital Technology", "department_id": "SADT630"}, 
-//         {"name": "School of Business", "department_id": "SADT420"}
-//     ], 
-//     "intakeSeasons": ["Fall", "Winter", "Summer"]
-// }
-
-// {"0": "A", "1": "l", "2": "l", "programs": [{"name": "Software Development", "program_id": "SADT-009"}, {"name": "Business Administration Diploma", "program_id": "SADT-012"}], "intakeYear": ["2023", "2024"], "departments": [{"name": "School of Advanced Digital Technology", "department_id": "SADT630"}], "intakeSeasons": ["Fall", "Spring"]}
 
 
     // return (
